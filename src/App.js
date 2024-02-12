3a647b6a1d0e732bf9944554174374256f62ff2a
jeongdonggi <dkrhank45@gmail.com>
2024-02-12 21:35:24 +0900

    app.js add base

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Detail from "./routes/Detail";
 
 export const App = () => {
   return (
-    <Router>
+    <Router basename="/MovieApp_nomard">
       <Routes>
         <Route path="/" element={<Home />} /> {/* 정적 라우터 지원 */}
         <Route path="/movie/:id"  element={<Detail />} /> {/* 동적 라우터 지원: [:id]로 적어야 됨 */}
